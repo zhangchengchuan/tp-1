2ab2f62d48adde92dcb95014a02cd7b0477959f6
Markus Lim <markuslim24@gmail.com>
2021-09-29 00:33:38 +0800

    add task skeleton in model package

---
+++ src/main/java/seedu/address/model/task/Event.java
@@ -0,0 +1,26 @@
+package seedu.address.model.task;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.time.LocalDateTime;
+
+
+public class Event extends Task {
+    private final LocalDateTime startDateTime;
+    private final LocalDateTime endDateTime;
+
+    public Event (Name name, Description description, Index index) {
+        super(name, description, index);
+    }
+
+    /**
+     * Constructor for an Event object with start and end DateTime.
+     */
+    public Event (Name name, Description description, Index index, LocalDateTime startDateTime,
+                  LocalDateTime endDateTime) {
+        super(name, description, index);
+        requireAllNonNull(startDateTime, endDateTime);
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+    }
+}
