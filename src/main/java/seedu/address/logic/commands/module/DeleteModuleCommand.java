4f04ab40a781311354516a075e84438209e0c72f
Tianqi-Zhu <tianqi_zhu2002@qq.com>
2021-10-07 04:43:39 +0800

    update add module and delete module class

--- src/main/java/seedu/address/logic/commands/module/DeleteModuleCommand.java
+++ src/main/java/seedu/address/logic/commands/module/DeleteModuleCommand.java
@@ -1,15 +1,55 @@
 package seedu.address.logic.commands.module;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.module.Module;
 
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Deletes a module identified using it's displayed index from the task manager.
+ */
 public class DeleteModuleCommand extends Command {
     public static final String COMMAND_WORD = "deleteMod";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the module identified by the index number used in the displayed module list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    private final Index targetIndex;
+
+    public static final String MESSAGE_DELETE_MODULE_SUCCESS = "Deleted Module: %1$s";
+
+    public DeleteModuleCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return null;
+        requireNonNull(model);
+        List<Module> lastShownList = model.getFilteredModuleList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_MODULE_DISPLAYED_INDEX);
+        }
+
+        Module moduleToDelete = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteModule(moduleToDelete);
+        return new CommandResult(String.format(MESSAGE_DELETE_MODULE_SUCCESS, moduleToDelete));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DeleteModuleCommand // instanceof handles nulls
+                && targetIndex.equals(((DeleteModuleCommand) other).targetIndex)); // state check
     }
 }
