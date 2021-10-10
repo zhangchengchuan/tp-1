96a1e9489628004cbaa88f8cfa77f1007f5766ec
Tianqi-Zhu <tianqi_zhu2002@qq.com>
2021-10-10 21:14:24 +0800

    add module related test skeleton

--- src/main/java/seedu/address/model/link/UniqueLinkList.java
+++ src/main/java/seedu/address/model/link/UniqueLinkList.java
@@ -1,4 +1,7 @@
 package seedu.address.model.link;
 
+/**
+ * TODO: V1.3
+ */
 public class UniqueLinkList {
 }
---
+++ src/test/java/seedu/address/logic/parser/module/DeleteModuleCommandParserTest.java
@@ -0,0 +1,4 @@
+package seedu.address.logic.parser.module;
+
+public class DeleteModuleCommandParserTest {
+}
