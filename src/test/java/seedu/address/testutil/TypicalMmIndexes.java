74955c8a0171149c0fa63200f4efbfb2ed5d1be0
Tianqi-Zhu <tianqi_zhu2002@qq.com>
2021-10-11 19:20:01 +0800

    add remaining tests for deleteMod and uniqueModuleList

---
+++ src/test/java/seedu/address/testutil/TypicalMmIndexes.java
@@ -0,0 +1,9 @@
+package seedu.address.testutil;
+
+import seedu.address.commons.core.index.Index;
+
+public class TypicalMmIndexes {
+    public static final Index INDEX_FIRST = Index.fromOneBased(1);
+    public static final Index INDEX_SECOND = Index.fromOneBased(2);
+    public static final Index INDEX_THIRD = Index.fromOneBased(3);
+}
