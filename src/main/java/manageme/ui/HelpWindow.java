218b60f8fae8d4a1a4a07053a4a9835010b8fc0c
Markus Lim <markuslim24@gmail.com>
2021-10-21 16:29:11 +0800

    fix checkstyle

--- src/main/java/manageme/ui/HelpWindow.java
+++ src/main/java/manageme/ui/HelpWindow.java
@@ -5,7 +5,6 @@ import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.stage.Stage;
