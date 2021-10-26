f29564a441eca2f12c3b1d05c9130ec21c0e5cfc
Tianqi-Zhu <tianqi_zhu2002@qq.com>
2021-10-26 18:34:39 +0800

    fix style

--- src/main/java/manageme/model/link/LinkAddress.java
+++ src/main/java/manageme/model/link/LinkAddress.java
@@ -37,18 +37,19 @@ public class LinkAddress {
         try {
             if (os.contains("win")) {
                 // this doesn't support showing urls in the form of "page.html"
-                rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
+                rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
             } else if (os.contains("mac")) {
                 rt.exec("open " + url);
             } else if (os.contains("nix") || os.contains("nux")) {
                 // Do a best guess on unix until we get a platform independent way
                 // Build a list of browsers to try, in this order.
-                String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
-                        "netscape", "opera", "links", "lynx"};
+                String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror", "netscape", "opera",
+                        "links", "lynx"};
                 // Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
                 StringBuffer cmd = new StringBuffer();
-                for (int i=0; i < browsers.length; i++)
-                    cmd.append((i == 0  ? "" : " || ") + browsers[i] + " \"" + url + "\" ");
+                for (int i = 0; i < browsers.length; i++) {
+                    cmd.append((i == 0 ? "" : " || ") + browsers[i] + " \"" + url + "\" ");
+                }
                 rt.exec(new String[] { "sh", "-c", cmd.toString() });
             }
         } catch (Exception e) {
@@ -66,7 +67,7 @@ public class LinkAddress {
             } catch (IOException e) {
                 throw new FileNotOpenException();
             }
-        } else if(! Desktop.isDesktopSupported()) {
+        } else if (! Desktop.isDesktopSupported()) {
             System.out.println("java desktop not supported");
             throw new DesktopNotSupportException();
         } else {
