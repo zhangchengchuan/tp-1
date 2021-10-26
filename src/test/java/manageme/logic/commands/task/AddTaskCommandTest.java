54ff42fd0fbdd9653684d98bc40ddee3ff0665b2
Tianqi-Zhu <tianqi_zhu2002@qq.com>
2021-10-26 21:12:12 +0800

    goodby persons

--- src/test/java/manageme/logic/commands/task/AddTaskCommandTest.java
+++ src/test/java/manageme/logic/commands/task/AddTaskCommandTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.Test;
 
 import javafx.collections.ObservableList;
 import manageme.commons.core.GuiSettings;
+import manageme.commons.core.index.Index;
 import manageme.logic.commands.CommandResult;
 import manageme.logic.commands.exceptions.CommandException;
 import manageme.model.ManageMe;
@@ -23,6 +24,7 @@ import manageme.model.Model;
 import manageme.model.ReadOnlyManageMe;
 import manageme.model.ReadOnlyUserPrefs;
 import manageme.model.link.Link;
+import manageme.model.link.LinkModule;
 import manageme.model.module.Module;
 import manageme.model.task.Task;
 import manageme.testutil.TaskBuilder;
@@ -140,6 +142,16 @@ public class AddTaskCommandTest {
 
         @Override
         public void setLink(Link target, Link editedLink) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void openLink(Link target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Link deleteModLink(LinkModule linkModule, Index index) {
             throw new AssertionError("This method should not be called.");
         }
 
