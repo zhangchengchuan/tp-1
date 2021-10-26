d4a775211f5d824fd48f04a5e1fcc03fe5087841
Tianqi-Zhu <tianqi_zhu2002@qq.com>
2021-10-26 18:25:52 +0800

    fix style

--- src/main/java/manageme/model/link/LinkAddress.java
+++ src/main/java/manageme/model/link/LinkAddress.java
@@ -34,37 +34,26 @@ public class LinkAddress {
         String os = System.getProperty("os.name").toLowerCase();
         Runtime rt = Runtime.getRuntime();
 
-        try{
-
+        try {
             if (os.contains("win")) {
-
                 // this doesn't support showing urls in the form of "page.html"
                 rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
-
             } else if (os.contains( "mac" )) {
-
-                rt.exec( "open " + url);
-
+                rt.exec("open " + url);
             } else if (os.contains( "nix") || os.contains( "nux")) {
-
                 // Do a best guess on unix until we get a platform independent way
                 // Build a list of browsers to try, in this order.
                 String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
                         "netscape","opera","links","lynx"};
-
                 // Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
                 StringBuffer cmd = new StringBuffer();
                 for (int i=0; i < browsers.length; i++)
-                    cmd.append( ( i==0  ? "" : " || " ) + browsers[i] +" \"" + url + "\" ");
-
+                    cmd.append((i == 0  ? "" : " || ") + browsers[i] + " \"" + url + "\" ");
                 rt.exec(new String[] { "sh", "-c", cmd.toString() });
-
             }
-
         } catch (Exception e) {
             throw new LinkNotOpenException();
         }
-
     }
 
     public static void openFile(URI uri) throws DesktopNotSupportException, FileNotOpenException {
